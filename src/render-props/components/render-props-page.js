7c9927d6f11a7d2a3f652e7da3ae720253213c8f
Steven <stevengf14@gmail.com>
2023-03-22 21:38:08 -0500

    Final Render Props

--- src/render-props/components/render-props-page.js
+++ src/render-props/components/render-props-page.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ErrorBoundary } from "./error-boundary";
+import { FinalErrorBoundary } from "./final-error-boundary";
 
 const MyBug = () => {
   const [isError, setIsError] = useState(false);
@@ -9,7 +10,7 @@ const MyBug = () => {
   };
 
   if (isError) {
-    throw new Error(":(");
+    throw new Error("nani?");
   }
 
   return <button onClick={handleCrash}>Crash app</button>;
@@ -21,5 +22,16 @@ export const RenderPropsPage = () => (
     <ErrorBoundary>
       <MyBug />
     </ErrorBoundary>
+    <hr></hr>
+    <h2>Example with Render Props</h2>
+    <FinalErrorBoundary
+      render={(error) => (
+        <p>
+          Oops! D: <strong>{`${error.message}`}</strong>
+        </p>
+      )}
+    >
+      <MyBug />
+    </FinalErrorBoundary>
   </>
 );
