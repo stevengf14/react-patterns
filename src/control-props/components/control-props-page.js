887470115055e23e3acc8c6ac50f0bdd08881b3e
Steven <stevengf14@gmail.com>
2023-03-29 23:38:07 -0500

    Control Props example

--- src/control-props/components/control-props-page.js
+++ src/control-props/components/control-props-page.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { LikeButton } from "./like-button";
+import { FinalLikeButton } from "./final-like-button";
 
-export const ControlPropsPage = () => (
-  <>
-    <h2>Example without Control Props</h2>
-    <LikeButton cb={(likes) => likes + 10} />
-    <hr></hr>
+export const ControlPropsPage = () => {
+  const [counter, setCounter] = useState(0);
+  const handleUpdateCounter = () => {
+    setCounter(counter + 5);
+  };
+  const handleChange = ({ target: { value } }) => {
+    if (value === "like") {
+      setCounter(counter + 1);
+    }
+  };
+  return (
+    <>
+      <h2>Example without Control Props</h2>
+      <LikeButton cb={(likes) => likes + 10} />
+      <hr></hr>
 
-    <h2>Example with Control Props</h2>
-  </>
-);
+      <h2>Example with Control Props</h2>
+      <input type="text" onChange={handleChange} />
+      <FinalLikeButton value={counter} setValue={handleUpdateCounter} />
+    </>
+  );
+};
