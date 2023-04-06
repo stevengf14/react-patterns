18c434216276dcc9897ac4ede62f359a2fe0fdf0
Steven <stevengf14@gmail.com>
2023-04-05 21:42:15 -0500

    props getter with render props

--- src/props-getters/components/props-getters-page.js
+++ src/props-getters/components/props-getters-page.js
@@ -1,3 +1,4 @@
+import { FinalFormWithRenderProps } from "./final-form-with-render-props";
 import { FormWithRenderProps } from "./form-with-render-props";
 
 export const PropsGetterPage = () => {
@@ -7,6 +8,10 @@ export const PropsGetterPage = () => {
     // logic...
     alert("change");
     handleChange(e);
+  };
+
+  const logChange = () => {
+    console.log("changed!");
   };
 
   return (
@@ -41,7 +46,34 @@ export const PropsGetterPage = () => {
       </FormWithRenderProps>
 
       <hr></hr>
-      <h2>Example with Props Getter</h2>
+      <h2>Example with Props Getter and Render Props</h2>
+      <FinalFormWithRenderProps initialState={{ name: "", jobTitle: "" }}>
+        {({ formValues, getInputProps, handleSubmit }) => (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <p>Name</p>
+              <input
+                type="text"
+                name="name"
+                value={formValues.name}
+                {...getInputProps({ onChange: logChange() })}
+              />
+            </div>
+            <div>
+              <p>Job Title</p>
+              <input
+                type="text"
+                name="jobTitle"
+                value={formValues.jobTitle}
+                {...getInputProps({ onChange: logChange() })}
+              />
+            </div>
+            <button style={{ margin: "8px 0" }} type="submit">
+              Submit
+            </button>
+          </form>
+        )}
+      </FinalFormWithRenderProps>
     </>
   );
 };
